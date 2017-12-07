d348e1edfded2d96ecf3fe5ffab3ec1c66e139c2
Aaron McPeek <arn1313@comcast.net>
2017-12-07 12:00:36 -0800

    holy cow its compiling

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import {BrowserRouter, Route, Redirect} from 'react-router-dom';
 import Navbar from '../navbar';
 import Aboutme from '../aboutme';
+import Header from '../header';
+import Projects from '../projects';
+import Footer from '../footer';
+
 
 class App extends React.Component {
   render() {
@@ -10,8 +14,10 @@ class App extends React.Component {
         <BrowserRouter>
           <div>
             <Navbar />
+            <Route exact path="/header" component={Header}/>
             <Route exact path="/aboutme" component={Aboutme}/>
-
+            <Route exact path="/projects" component={Projects}/>
+            <Route exact path="/footer" component={Footer}/>
           </div>
         </BrowserRouter>
       </div>
