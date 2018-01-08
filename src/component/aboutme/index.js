e8a8f2b4f22977b6f338178e8cf87ec0fbfc5a66
Aaron McPeek <arn1313@comcast.net>
2018-01-08 14:00:36 -0800

    aboutme

---
+++ src/component/aboutme/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {BrowserRouter, Route, Redirect} from 'react-router-dom';
+import Navbar from '../navbar';
+import Aboutme from '../aboutme';
+import Header from '../header';
+import Projects from '../projects';
+import Footer from '../footer';
+
+
+class AboutMe extends React.Component {
+  render() {
+    return (
+      <div className="about-me">
+        <BrowserRouter>
+          <div>
+            <Navbar />
+            <Route exact path="/header" component={Header}/>
+            <Route exact path="/aboutme" component={Aboutme}/>
+            <Route exact path="/projects" component={Projects}/>
+            <Route exact path="/footer" component={Footer}/>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}//nope
+
+export default AboutMe;
